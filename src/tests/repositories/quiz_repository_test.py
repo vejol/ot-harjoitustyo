94f63688217e1d1daa7422eda3810657b80b05f0
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-04-25 21:39:19 +0300

    Testit päivitetty vastaamaan ohjelman nykyistä tilaa

--- src/tests/repositories/quiz_repository_test.py
+++ src/tests/repositories/quiz_repository_test.py
@@ -10,15 +10,15 @@ class TestQuizRepository(unittest.TestCase):
         self.quiz_repository.delete_all()
     
     def test_save(self):
-        puzzle1 = Puzzle("Test Puzzle 1", ["one", "two", "three", "four", "five"], 1)
-        puzzle2 = Puzzle("Test Puzzle 2", ["six", "seven", "eight", "nine", "ten"], 1)
+        puzzle1 = Puzzle("Test Puzzle 1", ["ONE", "TWO", "THREE", "FOUR", "FIVE"], 1)
+        puzzle2 = Puzzle("Test Puzzle 2", ["SIX", "SEVEN", "EIGHT", "NINE", "TEN"], 1)
         quiz1 = Quiz("Test Quiz 1", [puzzle1])
         quiz2 = Quiz("Test Quiz 2", [puzzle2])
 
         self.quiz_repository.save(quiz1)
         self.quiz_repository.save(quiz2)
 
-        names = self.quiz_repository.find_all_names()
+        names = self.quiz_repository.find_quiz_names()
         quiz1 = self.quiz_repository.find_quiz(names[0])
         quiz2 = self.quiz_repository.find_quiz(names[1])
 
@@ -27,25 +27,25 @@ class TestQuizRepository(unittest.TestCase):
         self.assertEqual(quiz1.puzzles[0].name, "Test Puzzle 1")
         self.assertEqual(quiz2.puzzles[0].name, "Test Puzzle 2")
 
-        self.assertEqual(quiz1.puzzles[0].words, ["one", "two", "three", "four", "five"])
-        self.assertEqual(quiz2.puzzles[0].words, ["six", "seven", "eight", "nine", "ten"])
+        self.assertEqual(quiz1.puzzles[0].words, ["ONE", "TWO", "THREE", "FOUR", "FIVE"])
+        self.assertEqual(quiz2.puzzles[0].words, ["SIX", "SEVEN", "EIGHT", "NINE", "TEN"])
 
 
     def test_delete_all(self):
-        quizzes = self.quiz_repository.find_all_names()
+        quizzes = self.quiz_repository.find_quiz_names()
         self.assertEqual(len(quizzes), 0)
 
     
     def test_find_all_names(self):
-        puzzle1 = Puzzle("Test Puzzle 1", ["one", "two", "three", "four", "five"], 1)
-        puzzle2 = Puzzle("Test Puzzle 2", ["six", "seven", "eight", "nine", "ten"], 1)
+        puzzle1 = Puzzle("Test Puzzle 1", ["ONE", "TWO", "THREE", "FOUR", "FIVE"], 1)
+        puzzle2 = Puzzle("Test Puzzle 2", ["SIX", "SEVEN", "EIGHT", "NINE", "TEN"], 1)
         quiz1 = Quiz("Test Quiz 1", [puzzle1])
         quiz2 = Quiz("Test Quiz 2", [puzzle2])
 
         self.quiz_repository.save(quiz1)
         self.quiz_repository.save(quiz2)
 
-        names = self.quiz_repository.find_all_names()
+        names = self.quiz_repository.find_quiz_names()
 
         self.assertTrue(len(names) == 2)
         self.assertEqual(quiz1.puzzles[0].name, "Test Puzzle 1")
--- src/tests/services/game_service_test.py
+++ src/tests/services/game_service_test.py
@@ -10,18 +10,6 @@ class TestGameService(unittest.TestCase):
         quiz = Quiz("Test Quiz", [puzzle])
         self.game_service = GameService(quiz)
 
-    def test_turn_default_value_is_1(self):
-        self.assertEqual(self.game_service.turn, 1)
-    
-    def test_turn_changes_to_2(self):
-        self.game_service.change_turn()
-        self.assertEqual(self.game_service.turn, 2)
-
-    def test_turn_changes_back_to_1(self):
-        self.game_service.change_turn()
-        self.game_service.change_turn()
-        self.assertEqual(self.game_service.turn, 1)
-
     def test_adding_point_to_team1_works(self):
         self.game_service.add_point_team1()
         self.assertEqual(self.game_service.team1_points, 1)
