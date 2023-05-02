11471191f51acaa131eb856e7b3a1064f76cb41b
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-05-02 20:53:08 +0300

    Käyttöliittymään hahmoteltu visailunluomistoiminto

--- src/services/management_service.py
+++ src/services/management_service.py
@@ -3,16 +3,26 @@ from entities.puzzle import Puzzle
 from entities.quiz import Quiz
 
 class ManagementService:
+    """Luokka, joka vastaa sovelluksen logiikasta."""
 
     def __init__(self):
+        """Luokan konstruktori. Luo uuden sovelluksen logiikasta vastaavan palvelun."""
         self._quiz_repository = QuizRepository()
 
 
     def add_quiz(self, quiz):
+        """Lisää uuden visailun
+
+        Args:
+            quiz: Lisättävä Quiz-olio.
+        """
+
         self._quiz_repository.save(quiz)
 
 
     def add_default_quizzes(self):
+        """Lisää sovellukseen mallivisailut."""
+
         puzzle1 = Puzzle("Maamme-laulu", ["laaksoa", "ei", "kukkulaa", "ei", "vettä"], 1)
         puzzle2 = Puzzle("Petteri Punakuono", ["pitkä", "on", "taival", "valoton,", "Petteri"], 1)
 
@@ -21,14 +31,34 @@ class ManagementService:
 
 
     def find_quiz_names(self):
+        """Palauttaa tallennettujen visailujen nimet.
+
+        Returns:
+            Lista, joka sisältää tallennettujen visailujen nimet.
+        """
+
         return self._quiz_repository.find_quiz_names()
 
 
     def find_all_quizzes(self):
+        """Palauttaa kaikki tallennetut visailut.
+
+        Returns:
+            Lista, joka sisältää kaikki ohjelmaan tallennetut visailut Quiz-olioina.
+        """
         return self._quiz_repository.find_all_quizzes()
 
 
     def find_quiz(self, name):
+        """Palauttaa halutun visailun nimen perusteella.
+
+        Args:
+            name: Halutun visailun nimi merkkijonona.
+
+        Returns:
+            Quiz-olio, joka vastaa parametriksi annettua visailun nimeä.
+        """
+
         return self._quiz_repository.find_quiz(name)
 
 
--- src/ui/create_quiz_view.py
+++ src/ui/create_quiz_view.py
@@ -1,23 +1,122 @@
-from tkinter import ttk, constants
+from tkinter import ttk, constants, Frame
 
+class PuzzleQueryList:
+    """Arvoitusten listaamisesta vastaava näkymä."""
+     
+    def __init__(self, root, query_count):
+        """Luokan konstruktori. Luo uuden arvoituksia listaavan näkymän.
 
-class CreateQuizView:
+        Args:
+            root: TKinter-elementti, joka sisältää ohjelman ikkunan.
+            query_count: Alustettavien visailupohjien lukumäärä.
+        """
 
-    def __init__(self, root, handle_opening_view):
         self._root = root
-        self._handle_opening_view = handle_opening_view
+        self._query_count = query_count
         self._frame = None
 
         self._initialize()
 
+    def pack(self):
+        """Näyttää näkymän."""
+        self._frame.pack(fill=constants.X, padx=20, pady=20)
+    
     def destroy(self):
+        """Sulkee näkymän."""
+        self._frame.destroy()
+    
+    def _initialize_puzzle_query(self, order_no):
+        query_frame = Frame(
+            master=self._frame,
+            highlightbackground="grey",
+            highlightthickness=2)
+        
+        puzzle_label = ttk.Label(
+            master=query_frame, 
+            text=f"Arvoitus {order_no}",
+            font="Helvetica 10 bold"
+        )
+
+        puzzle_name_label = ttk.Label(
+            master=query_frame, 
+            text="Kappaleen nimi: "
+        )
+
+        puzzle_words_label = ttk.Label(
+            master=query_frame,
+            text="Piilotettavat sanat: "
+        )
+
+        puzzle_name_entry = ttk.Entry(master=query_frame)
+        
+        puzzle_label.grid(row=0, column=0, padx=10, pady=10, sticky=constants.EW)
+        puzzle_name_label.grid(row=1, column=0, padx=10, pady=10, sticky=constants.EW)
+        puzzle_name_entry.grid(row=1, column=1, columnspan=2, padx=10, pady=10, sticky=constants.EW)
+        puzzle_words_label.grid(row=2, column=0, padx=10, pady=0, sticky=constants.EW)
+
+        self._init_puzzle_word_entries(query_frame, 5)
+
+        query_frame.grid_columnconfigure(0, weight=1)
+        query_frame.pack(fill=constants.X)
+    
+    def _init_puzzle_word_entries(self, frame, count):
+        for i in range(count):
+            entry = ttk.Entry(master=frame, width=10)
+            label = ttk.Label(master=frame, text=f"sana{i+1}", font="Helvetica 8 italic")
+            
+            entry.grid(row=2, column=i+1, padx=10, pady=0)
+            label.grid(row=3, column=i+1, padx=5, pady=5)
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+
+        for i in range(self._query_count):
+            self._initialize_puzzle_query(i+1)
+
+
+class CreateQuizView:
+    """Luokka, joka vastaa visailujen luomiseen tarkoitetusta näkymästä."""
+
+    def __init__(self, root, handle_opening_view):
+        """Luokan konstruktori. Luo uuden visailujenluomisnäkymän.
+
+        Args:
+            root: TKinter-elementti, joka sisältää ohjelman ikkunan.
+            handle_opening_view: Arvo, jota kutsumalla pääsee takaisin aloitusnäkymään.
+        """
+
+        self._root = root
+        self._handle_opening_view = handle_opening_view
+        self._frame = None
+        self._puzzle_query_frame = None
+        self._puzzle_query_view = None
+
+        self._initialize()
+
+    def destroy(self):
+        """Sulkee näkymän."""
         self._frame.destroy()
 
     def pack(self):
-            self._frame.pack(fill=constants.X)
+        """Näyttää näkymän."""
+        self._frame.pack(fill=constants.X)
                 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
+        self._puzzle_query_frame = ttk.Frame(master=self._frame)
+
+        self._initialize_upper_part()
+        self._initialize_puzzle_query_list()
+        self._initialize_footer()
+
+        self._puzzle_query_frame.grid(
+            row=1,
+            column=0,
+            columnspan=2,
+            sticky=constants.EW
+        )
+
+    def _initialize_footer(self):
 
         cancel_button = ttk.Button(
             master=self._frame,
@@ -28,13 +127,36 @@ class CreateQuizView:
         save_button = ttk.Button(
             master=self._frame,
             text="Tallenna",
-            command=self._save_and_return
+            command=self._handle_save_and_return
         )
 
-        cancel_button.grid(row=0, column=0)
-        save_button.grid(row=0, column=1)
+        cancel_button.grid(row=3, column=0)
+        save_button.grid(row=3, column=1)
+
+    def _initialize_puzzle_query_list(self):
+        if self._puzzle_query_view:
+            self._puzzle_query_view.destroy()
+
+        self._puzzle_query_view = PuzzleQueryList(self._puzzle_query_frame, 2)
+
+        self._puzzle_query_view.pack()
 
 
-    def _save_and_return(self):
+    def _initialize_upper_part(self):
+
+        quiz_name_label = ttk.Label(
+            master=self._frame,
+            text="Visailun nimi:",
+            font="Helvetica 10 bold"
+        )
+
+        quiz_name_entry = ttk.Entry(master=self._frame)
+
+        quiz_name_label.grid(row=0, column=0, padx=5, pady=5, sticky=constants.E)
+        quiz_name_entry.grid(row=0, column=1, padx=5, pady=5, sticky=constants.EW)
+
+
+    def _handle_save_and_return(self):
          # Saving the new quiz to database
-         self._handle_opening_view()
+         self._puzzle_query_view.pack()
+         #self._handle_opening_view()
